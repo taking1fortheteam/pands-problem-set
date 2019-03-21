7f1f4378bed14ee290285a73eeb40ff54b7ecdc9
Aidan Conlon <ajconlon@gmail.com>
2019-03-21 10:27:43 +0000

    FInal working version with a checksum

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Aidan Conlon - 20 March 2019
 # This is the solution to Problem 1
 # A Program that asks the user to input any positive integer and outputs the
@@ -28,34 +27,3 @@ except ValueError:
     print("That is not an integer") # Print to screen it is not an integer and finish.
 
 
-=======
-# Aidan Conlon - 20 March 2019
-# This is the solution to Problem 1
-# A Program that asks the user to input any positive integer and outputs the
-# sum of all numbers between one and that number.
-
-#  Ask user to enter a positive integer
-User_Input = input("Enter positive Integer : ")
-
-# I need to decipher if it is an integer an dwhether it is, is it a positive one!
-
-
-# Check to see if entered characters are a number or string
-try:                                    # Try the value entered as an integer? 
-    val = int(User_Input)               # Value entered is an integer (i.e. 3)
-    if val <= 0:
-        print("That is not a positive integer") # Print to screen it is not an integer and finish.
-        quit()
-    i = val                             # let i equal to integer value            
-    while (i > 0):                          # While loop to check for i is not 0
-        i = i-1                             # decrement i by 1
-        val = (val + i)                     # make val equal to old val plus value of i (i.e. 3 + 2) 
-    print ("The sum is" , val)          # Print to screen "The sum is" and the value stored by val.
-    quit()                              # Quit
-
-# If value entered is not an integer then:   
-except ValueError:              
-    print("That is not an integer") # Print to screen it is not an integer and finish.
-
-
->>>>>>> f2280d89067fbfe4f0fd13df3daf73ba0c998988
--- Problem2.py
+++ Problem2.py
@@ -1,11 +1,16 @@
-# Aidan Conlon - 20 March 2019
+# Aidan Conlon - 21 March 2019
 # This is the solution to Problem 2
 # Write a program that outputs whether or not today is a day that begins with the
 # letter T.
 
-import datetime
+import datetime                                     # Import the date and time
+                                                    # Do a  check to see if the current day is 1 - TUesday or 3 - Thursday
+if datetime.datetime.today().weekday() == 1 or 3:   # Monday is 0 and Sunday is 6
+  print("Yay! The day begins with a T.")                # If result is Tuesday or Thursday print to screen comment.
+else:                                               # If day is not Tuesday or Thursday then 
+  print("Unfortunately it does not begin with a T.")    # Print comment to screen
 
-if datetime.datetime.today().weekday() == 1 or 3: # Modnday is 0 and Sunday is 6
-  print("Yay! The day begins with a T.")
-else:
-  print("Unfortunately it does not begin with a T.")
+
+x = datetime.datetime.now()                         # As a checksum - let x equal to the current date / time.
+print(x.strftime("%A"))                             # Print to screen the Day - strftime converts datetime to a  string
+                                                    # %A reprsents the full Day ie. Wednesday instead of Wednesday to be printed.
