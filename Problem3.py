10483c50bb67d2d57905c3e01c4bc8269568cd0e
Aidan Conlon <ajconlon@gmail.com>
2019-03-21 11:29:36 +0000

    FInal version of Problem 3

--- Problem3.py
+++ Problem3.py
@@ -3,8 +3,15 @@
 # Write a program that prints all numbers between 1,000 and 10,000 that are divisible
 # by 6 but not 12.
 
-i = 9000:
-x = 1000:
+i = 1000                            # Set i to 1000
+x = 1000                            # Set x to 1000
 
-while (i < 10000):
-    if 
+#                                    Print to screen following comment
+print("The Values divisible by 6 but not divisible by 12 between 1,000 & 10,000 are:")
+
+while i <= 10000:                   # While loop - while i is less than 10001 continue with this loop
+    if x % 6 == 0 and x % 12 != 0:         # if x is divisible by 6 (no remainder) AND not divisible by 12 (remainder)
+        print(x)                                # print the value of x to the screen
+    i = i + 1                               # increment the value of i (for next iteration)
+    x = x + 1                                # increment value of x (for next iteration)
+quit()                              # Finish
