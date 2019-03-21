b327a2a0cde1faa80cca958124c87ab966718a4b
Aidan Conlon <ajconlon@gmail.com>
2019-03-21 10:38:49 +0000

    this is the beginning of problem 3

---
+++ Problem3.py
@@ -0,0 +1,10 @@
+# Aidan Conlon - 21 March 2019
+# This is the solution to Problem 3
+# Write a program that prints all numbers between 1,000 and 10,000 that are divisible
+# by 6 but not 12.
+
+i = 9000:
+x = 1000:
+
+while (i < 10000):
+    if 
